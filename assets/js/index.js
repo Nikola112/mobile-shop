d8af904210124c4b73178116fcb94e68b7e190c1
Nikola Jovic <nikjovke1@gmail.com>
2019-01-26 15:26:49 +0100

    Best deal added.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,11 @@
 $(function()
 {
-    
+    // this will always be 3 more days
+    let expireDate = new Date();
+    expireDate.setDate(expireDate.getDate() + 3);
+
+    $("#best-deal-countdown .text").countdown(expireDate, function(event) 
+    {
+        $(this).text(event.strftime('%I:%M:%S'));
+    });
 });
